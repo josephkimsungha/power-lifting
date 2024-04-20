7651b1438ae5bbe4761444d3e8feeba7bdde394a
Zain Afzal <zain.afz@gmail.com>
2024-04-20 22:38:54 +1000

    Add basic main menu (#10)

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,2 @@
-import { Application } from 'pixi.js';
-import { Controller } from './controller';
+import './main-menu.ts';
 
-class Game {
-  private app: Application;
-  private controller: Controller;
-
-  constructor() {
-    this.app = new Application();
-    this.controller = new Controller(this.app);
-  }
-
-  async start(): Promise<void> {
-    await this.app.init({ resizeTo: window });
-    document.body.appendChild(this.app.canvas);
-
-    this.controller.start();
-  }
-}
-
-const game = new Game();
-game.start();
---
+++ src/main-menu.ts
@@ -0,0 +1,44 @@
+import { LitElement, css, html } from 'lit';
+import { customElement, state } from 'lit/decorators.js';
+import { game } from './game';
+
+@customElement('main-menu')
+export class MainMenu extends LitElement {
+  static styles = css`
+    :host {
+      width: 100vw;
+      height: 100vh;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+    }
+  `;
+
+  @state()
+  private settingsOpen = false;
+
+  protected override render() {
+    if (this.settingsOpen) {
+      return this.renderSettings();
+    }
+
+    return html`
+      <h1> Power Lifter </h1>
+      <button @click=${this.startGame}> Play </button>
+      <button @click=${() => (this.settingsOpen = true)}> Settings </button>
+    `;
+  }
+
+  private renderSettings() {
+    return html`
+      <h1> Settings </h1>
+      <button @click=${() => (this.settingsOpen = false)}> Back </button>
+    `;
+  }
+
+  private startGame() {
+    this.remove();
+    game.start();
+  }
+}
