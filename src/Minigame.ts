61cef3302238aaa16a5fedb7769416f074e8b173
Zain Afzal <zain.afz@gmail.com>
2024-04-20 15:54:48 +1000

    Add in manager, make default minigame more interesting

--- src/Minigame.ts
+++ src/Minigame.ts
@@ -1,20 +1,28 @@
 import { Application, Container, Graphics } from "pixi.js";
 
+export interface MinigameDelegate {
+  finished: (passed: boolean) => void;
+}
+
 /** Base class all minigames extend */
 export class Minigame {
   private container: Container;
 
-  constructor(private readonly app: Application) {
+  constructor(private readonly app: Application, private readonly minigameDelegate: MinigameDelegate) {
     this.container = new Container();
   }
 
-  finishMinigame(passed: boolean) {
-    console.log('Minigame Finished', passed);
+  private finishMinigame(passed: boolean) {
+    // Show the player they've won or lost.
+    this.minigameDelegate.finished(passed);
   }
 
-  populateContainer() {
+  protected populateContainer() {
     const square = new Graphics();
-    square.rect(50, 50, 100, 100);
+    const x = (this.app.screen.width - 100) * Math.random();
+    const y = (this.app.screen.height - 100) * Math.random();
+
+    square.rect(x, y, 100, 100);
     square.fill(0xde3249);
     square.eventMode = 'static';
     square.on('pointerdown', () => {
@@ -25,6 +33,7 @@ export class Minigame {
   }
 
   attach() {
+    this.populateContainer();
     this.app.stage.addChild(this.container);
   }
 
