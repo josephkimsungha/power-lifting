c16b5e0a61aacf8e9cd5a732052644e26193a0ec
Zain Afzal <zain.afz@gmail.com>
2024-04-21 22:51:18 +1000

    move to audio folder (#17)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import "./main-menu.ts";
-import { audioController } from "./audio_controller.js";
+import { audioController } from "./audio/audio_controller.js";
 
 audioController.playTitleScreenMusic();
